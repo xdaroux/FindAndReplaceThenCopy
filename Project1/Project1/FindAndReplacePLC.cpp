78bf707c3dcf862e5e72ec448ea1cf1faf9bc712
xdaroux <dartagnan.leonard@outlook.com>
2020-07-29 11:36:35 -0400

    Modifier les commentaire dans CMD

--- Project1/Project1/FindAndReplacePLC.cpp
+++ Project1/Project1/FindAndReplacePLC.cpp
@@ -24,13 +24,13 @@ int main()
 	Plc MonPlcTest = Plc();
 
 	
-	if (MonFichierEntree.enregistreFichierIn())  // Si il ny a pas derreru continue 
+	if (MonFichierEntree.enregistreFichierIn())  // S'il n y a pas d'erreur on continue 
 	{
 		MonFichierEntree.affichierFichierIn();
 		MonFichierEntree.separationSectionFichierIn();
-		//MonFichierEntree.afficherDonnee();
+		
 		MonFichierEntree.genereTypeDeDonne();
-		//MonFichierEntree.afficherSeparationTypeDonne();
+		
 
 		MonUsine.ajouterLesPlc(MonFichierEntree.nomUsine, MonFichierEntree.TD_nomPlc, MonFichierEntree.nbPlc, MonFichierEntree.TD_typeDonnee, MonFichierEntree.nbtypeDonnee);
 		MonUsine.Usine_FindAndReplacePlcName(MonFichierEntree.nomToReplace);
@@ -41,20 +41,6 @@ int main()
 	}
 	system("PAUSE");
 	
-		//
-		//MonPlcTest.enregistrerPlcGroupeDeDonnee(MonFichierEntree.sortirToutLesTypeDeDonnee()); // plc test
-
-		//cout << "Ici j'affiche mon AI dans mon plc Test " << endl;
-		//MonPlcTest.plcGroupeDeDonnee[0].afficherSeparationDesDonnees();
-		//
-		//MonPlcTest.plcGroupeDeDonnee[0].~TypeDeDonnee(); //destruction faut je regarde si sa detruit l,autre aussi car si oui alors c,est passser par adresse et sa c pas bon car moi je veux des copies
-		//cout << "Ici j'ai detruit le AI pour mon plc" << endl; ///c'est bien passer par copies 
-		//
-		//cout << "Ici j<affiche la donne que je ne veut pas perdre tres important" << endl;
-		//MonFichierEntree.afficherSeparationTypeDonne();
-
-		//cout << "Ici jaffiche mon plc dont jai demolie un itemps sa devrai pt ici environ " << endl;  //// c'est parfait sa fonctionne numeros 1 cette partie !!!
-		//MonPlcTest.plcGroupeDeDonnee[0].afficherSeparationDesDonnees();
 }
 
 
